888ff757e85dec59ab8107984f2a2d57f8f5d3bc
frenzie24 <kevincharlesgross@gmail.com>
2024-08-25 15:38:15 -0500

    added basic styling to navbar component. implemented navbar component

--- oceanview-cleaning/src/App.jsx
+++ oceanview-cleaning/src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-
-import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import Home from './views/Home';
-import CarouselPage from './views/Carousel';
-import Contact from './views/Contact';
-import Prices from './views/Prices';
 import NavBar from './components/Navbar';
 
+import './App.css'
 function App() {
 
 
   return (
     <div>
       <NavBar />
-      <Outlet></Outlet>
+      <Outlet />
 
 
     </div>
