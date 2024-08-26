14d5d1f1d48672f118c249aa7e23af5a128d1b3d
frenzie24 <kevincharlesgross@gmail.com>
2024-08-26 00:21:29 -0500

    correct formatting issues on prices and contact components.  added global h2 definition

--- oceanview-cleaning/src/views/Contact.jsx
+++ oceanview-cleaning/src/views/Contact.jsx
@@ -3,11 +3,14 @@ import FBLink from '../components/FBLink';
 
 function Contact() {
   return (
-    <div className='mx-auto text-2xl text-white text-center drop-shadow-[0_1.5px_1.5px_rgba(0,0,0,1.0)]'>
-      <h1 className='font-bold'>Contact Us</h1>
-      <p><a href="mailto:oceanviewcleans@gmail.com" className="text-white hover:underline">oceanviewcleans@gmail.com</a></p>
+    <div className='flex flex-row flex-wrap justify-center text-2xl text-white text-center drop-shadow-[0_1.5px_1.5px_rgba(0,0,0,1.0)] pb-10'>
+      <h2 className='font-bold'>Contact Us</h2>
       <p><a href="tel:+18508141566" className="text-white hover:underline">(850) 814-1566</a></p>
+      <p><a href="mailto:oceanviewcleans@gmail.com" className="text-white hover:underline">oceanviewcleans@gmail.com</a></p>
 
+
+      <p>Connect with us on Facebook</p>
+      <FBLink />
     </div>
   );
 }
