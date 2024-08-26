54906999d977a6c5cb7ade88614c862425543090
frenzie24 <kevincharlesgross@gmail.com>
2024-08-26 00:07:26 -0500

    implemented header and nav bar formatting for mobile dimenstions

--- oceanview-cleaning/src/components/Navbar.jsx
+++ oceanview-cleaning/src/components/Navbar.jsx
@@ -7,10 +7,10 @@ export default function NavBar() {
 
     return (
         <nav >
-            <ul className='w-fit flex flex-row flex-wrap text-2xl text-black bg-sky-700 rounded-lg [&_li]:p-2 [&_*]:text-white [&_*]:hover:text-white '>
-                <li className='hover:bg-sky-800 hover:rounded-lg'><Link to="/">Home</Link></li>
-                <li className='hover:bg-sky-800 hover:rounded-lg'><Link to="/carousel">Examples</Link></li>
-                <li className='hover:bg-sky-800 hover:rounded-lg'><Link to="/pricesandcontact">Contact and Pricing</Link></li>
+            <ul className='w-fit flex flex-row flex-wrap bg-sky-700 rounded-lg [&_li]:p-2 [&_*]:text-white [&_*]:hover:text-white '>
+                <li className='text-xs md:text-lg hover:bg-sky-800 hover:rounded-lg'><Link to="/">Home</Link></li>
+                <li className='text-xs md:text-lg hover:bg-sky-800 hover:rounded-lg'><Link to="/carousel">Examples</Link></li>
+                <li className='text-xs md:text-lg hover:bg-sky-800 hover:rounded-lg'><Link to="/pricesandcontact">Contact and Pricing</Link></li>
             </ul>
         </nav>
     )
