232f54061f7d9f931c22a987497c4b2fe0ac13f7
frenzie24 <kevincharlesgross@gmail.com>
2024-08-29 22:39:36 -0500

    final changes, added full downloaded copies of example images added functionality to home screen, the beach background has a chance to be different every time a user visits

--- oceanview-cleaning/src/App.jsx
+++ oceanview-cleaning/src/App.jsx
@@ -5,12 +5,24 @@ import NavBar from './components/Navbar';
 
 import './App.css'
 import Header from './components/Header';
+import { useState } from 'react';
 function App() {
 
+  const images = [
+    'beach1.jpg',
+    'beach2.jpg',
+    'beach3.jpg',
+    'beach4.jpg',
+    'beach5.jpg',
+    'beach6.jpg',
+    'beach7.jpg'
+  ];
+
+  const randomImg = images[Math.floor(Math.random() * images.length)];
 
   return (
     <div className='w-screen h-screen flex flex-row flex-wrap '>
-      <div className='bg-image' />
+      <div className='bg-image'><img src={`./assets/backgrounds/${randomImg}`}></img></div>
       <Header />
       <Outlet />
 
